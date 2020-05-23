7066ab29fa734084e5e3ba67373101df8d1be022
Justin <schiller.justin@gmail.com>
2020-05-22 19:48:48 -0500

    Setting up NodeMailer configuration to allow users to contact me by email securely

--- app.js
+++ app.js
@@ -1,26 +1,53 @@
 const    express = require('express'),
          app = express(),
+         bodyParser = require('body-parser'),
          nodemailer = require('nodemailer'),
          path = require('path');
          port = process.env.PORT || 3000;
 
          require('dotenv').config();
 
-app.use(express.static(__dirname + "/public"));
 
-// const transporter = nodemailer.createTransport({
-//    service: 'gmail',
-//    auth: {
-//       user: process.env.USER,
-//       pass: process.env.PASSWORD
-//    }
-// });
+app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: true }));
 
 
 app.get('/', (req, res) => {
    res.render('index.ejs');
 });
 
+
+
+const transporter = nodemailer.createTransport({
+   service: 'gmail',
+   secure: false,
+   requireTLS: true,
+   auth: {
+      user: process.env.USER,
+      pass: process.env.PASSWORD
+   }
+});
+
+app.post('/contact', (req, res) => {
+   console.log(req.body.email);
+   const mailOptions = {
+      from: `${req.body.email} <${process.env.USER}>`,
+      to: process.env.USER,
+      subject: 'WebDev Portfolio Message',
+      text: req.body.message,
+      html: `<p>${req.body.name} at ${req.body.email} sent you the following message:</p>
+             <p>${req.body.message}</p>`
+   };
+
+   transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+          return console.log(error.message);
+      } else {
+          console.log("Contact request email sent!");
+      }
+  });
+
+});
 
 
 
